5b8a7422dc5527f565757b2206e33df099e6d878
庄意强 <409853036@qq.com>
2019-12-27 17:46:22 +0800

    第一页对比

--- Scripts/Android/Public Module/Transition Page/Transition Page Comparison/transition_page_one_comparison/Script1576573462545.groovy
+++ Scripts/Android/Public Module/Transition Page/Transition Page Comparison/transition_page_one_comparison/Script1576573462545.groovy
@@ -6,7 +6,7 @@ import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
 import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory as MobileDriverFactory
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.testcase.TestCase as TestCase
 import com.kms.katalon.core.testdata.TestData as TestData
@@ -14,5 +14,16 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import freemarker.core.BuiltInsForStringsMisc.booleanBI as booleanBI
 import internal.GlobalVariable as GlobalVariable
+
+//'调整图像对比相似度百分比'
+//MobileDriverFactory.getDriver().setSetting(Setting.IMAGE_MATCH_THRESHOLD, 0.6)
+
+
+'通过图像空间查看过渡页是否一致'
+boolean isPresent = Mobile.verifyElementExist(findTestCase("Object Repository/Android/Transition Page Element/transition_page_one_layout"), GlobalVariable.G_Timeout, FailureHandling.CONTINUE_ON_FAILURE)
+
+"判断是都相同"
+assert isPresent==true
 
--- Test Suites/Android/Teacher Test Suite/Ordinary_teacher.ts
+++ Test Suites/Android/Teacher Test Suite/Ordinary_teacher.ts
@@ -19,13 +19,13 @@
    <testCaseLink>
       <guid>4b906a2f-89c8-4e33-b7e5-62e2b5424b44</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Android/Public Module/Choose City/choose_city_jiexi</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>b44a84e1-ff94-40ec-b8f3-0d5f5b3c1e3d</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Android/Public Module/Login And Page/login</testCaseId>
       <variableLink>
          <testDataLinkId></testDataLinkId>
@@ -39,5 +39,11 @@
          <value></value>
          <variableId>f5fcfc94-1a67-4519-aa60-65443fbe08b9</variableId>
       </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d9707755-deb3-4e43-8ca6-bc9f6f6f6bcd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Android/Public Module/Transition Page/Transition Page Comparison/transition_page_one_comparison</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
