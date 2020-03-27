80860b956bbe42ea86a4eaed3f36c9d42b8ff70e
庄意强 <409853036@qq.com>
2020-03-27 14:28:13 +0800

    1.教师成绩列表

---
+++ Object Repository/Api/Mobile Api/Campus/Course Schedule/class_course_schedule.rs
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description>班级课程表数据</description>
+   <name>class_course_schedule</name>
+   <tag></tag>
+   <elementGuidId>1468730c-fb57-4b2c-ae94-b1cc0ab9ca08</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization</name>
+      <type>Main</type>
+      <value>Bearer ${GlobalVariable.access_token}</value>
+   </httpHeaderProperties>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>${GlobalVariable.MobileHost}/user_profile/api/upf/course_schedule/check?klass_id=${GlobalVariable.class_id}</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceFunction></soapServiceFunction>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+&quot;请求服务器成功:200&quot;
+if(WS.verifyResponseStatusCode(response, 200)){
+
+&quot;文本code值:200&quot;
+WS.verifyElementPropertyValue(response, 'code', 200)
+
+}</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
