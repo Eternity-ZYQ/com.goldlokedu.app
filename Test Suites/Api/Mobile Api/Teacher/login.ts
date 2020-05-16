f2238a24358c17ff3d05eaac5b091980c5b18d11
Administrator <Administrator@PC-20190203YURK>
2020-05-16 16:32:31 +0800

    1.测试套件及测试套件集合组装

---
+++ Test Suites/Api/Mobile Api/Teacher/login.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>登录用例测试套件</description>
+   <name>login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>93fbfbc4-9eb9-4a63-939f-fbd7860dbb50</testSuiteGuid>
+   <testCaseLink>
+      <guid>6b2339b0-555f-4531-97c4-29d618fd8c07</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Api/Mobile Api/Login And Logout/account_empty</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6feb9a57-00ea-4ced-8860-1a8145cae9f8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Api/Mobile Api/Login And Logout/account_password_error</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5f157757-b4e3-4c09-98b2-77e023ee7745</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Api/Mobile Api/Login And Logout/password_empty</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>41770cb1-e5fd-4672-9c97-01468fddee7a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Api/Mobile Api/Login And Logout/teacher_login_success</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
