a94fb2e41f95df409153f64d5b733963900f8d16
庄意强 <409853036@qq.com>
2020-05-20 18:07:02 +0800

    1.新高考接口参数错误修复

--- Object Repository/Api/Mobile Api/Campus/New college entrance examination/select_require_list_filter_province.rs
+++ Object Repository/Api/Mobile Api/Campus/New college entrance examination/select_require_list_filter_province.rs
@@ -19,7 +19,7 @@
    </httpHeaderProperties>
    <migratedVersion>5.4.1</migratedVersion>
    <restRequestMethod>GET</restRequestMethod>
-   <restUrl>http://192.168.2.243/content/front/gaokao/bySchool?from=${from}&amp;province=${province}&amp;size=${size}</restUrl>
+   <restUrl>${GlobalVariable.MobileHost}/content/front/gaokao/bySchool?from=${from}&amp;province=${province}&amp;size=${size}</restUrl>
    <serviceType>RESTful</serviceType>
    <soapBody></soapBody>
    <soapHeader></soapHeader>
